44a69ddf2e7ef66c62b66dfb80aaafca86607d68
chandan6372-khatua <chandankumarkhatua381@gmail.com>
2025-10-03 21:36:37 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,10 @@
+# Task 2: Sum of Integers from 1 to 50
+
+# Initialize sum
+total = 0
+
+# Use for loop to calculate sum
+for i in range(1, 51):
+    total += i
+
+print("The sum of integers from 1 to 50 is:", total)
